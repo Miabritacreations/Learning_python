323b1ce6b0a2dc6ce08856a84f32b0d4309a36e4
Miabrita <gichanabridgit@gmail.com>
2025-03-13 02:13:26 +0300

    added variables and changed file name

---
+++ hello.py
@@ -0,0 +1,33 @@
+#This is my first oythin program
+print("Hello world!")
+
+#these are strings
+first_name = "Bree"
+food = "pizza"
+email = "nyambekabridgit@gmail.com" 
+
+
+print(f"Hello {first_name}")
+print(f"I love {food}")
+print(f"Your email is: {email}")
+
+#integers
+
+age = 20
+quantity =3
+number_of_students = 30
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"Your class has {number_of_students} students")
+
+#floats
+
+price = 10.99
+gpa = 3.5
+distance = 7.7
+
+print(f"The price of bread is {price}")
+print(f"Your gpa is {gpa}")
+print(f"You ran {distance} km") 
+
