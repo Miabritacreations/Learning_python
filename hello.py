d7bcfa6ebcbc316472a3849255cd1096add2611a
Miabrita <gichanabridgit@gmail.com>
2025-03-27 12:01:11 +0300

    prompting user for name

--- hello.py
+++ hello.py
@@ -1,33 +1,46 @@
-#This is my first oythin program
+#This is my first python program
 print("Hello world!")
+#variables are containers or storage areas for holdong data. Variables include stirngs,integers, floats and boolean
 
 #these are strings
-first_name = "Bree"
+first_name = "Bridgit"
 food = "pizza"
 email = "nyambekabridgit@gmail.com" 
+school = "KCA University Main Campus"
 
 
 print(f"Hello {first_name}")
 print(f"I love {food}")
 print(f"Your email is: {email}")
+print(f"You are from {school}")
 
-#integers
+#integers(whole numbers) if you put floats in between quotation marks they will behave as strings
 
+year = 2025
 age = 20
 quantity =3
 number_of_students = 30
 
+print(f"We are in the year {year}")
 print(f"You are {age} years old")
 print(f"You are buying {quantity} items")
 print(f"Your class has {number_of_students} students")
 
-#floats
+#floats (decimal points)
 
 price = 10.99
 gpa = 3.5
 distance = 7.7
 
-print(f"The price of bread is {price}")
+print(f"The price of bread is Ksh.{price}")
 print(f"Your gpa is {gpa}")
 print(f"You ran {distance} km") 
+
+#boolean values usually true or false and  normally used in If or else statements If  a statement is true it can never be false and the latter is true
+
+
+
+
+
+
 
